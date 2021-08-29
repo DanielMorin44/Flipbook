675dc8f4b0528f828258febba95b65eb70c15729
aetherfox44 <danielmorin44@gmail.com>
2021-08-28 23:05:29 -0400

    Lock skybox
    Added Skybox Controller script to lock skybox to main camera

---
+++ Assets/Scripts/SkyboxController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+    public Camera mainCamera;
+    float x, y, z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        z = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = mainCamera.transform.position.x;
+        y = mainCamera.transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(x, y, z);
+    }
+}
