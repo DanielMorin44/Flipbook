d021bd4f1b7d67159f125c340960ccacf87a2377
ItsDobes <vikingdobes@gmail.com>
2025-03-10 15:15:22 -0400

    Added Death Animation, Hazard Tileset and Spikes
    Player now has a death animation and sound effect. This will freeze all player physics during death. Added a new tileset for hazards to replace the spikewall prefab. All tiles on the hazard tileset will kill the player. I had to manually adjust all the spikes by 0.25 offset to make them look good attached to walls and floors. I also adjusted the position of a few coins and flip tokens to make them less annoying to collect.

--- Assets/PlayerAudioManager.cs
+++ Assets/PlayerAudioManager.cs
@@ -11,6 +11,7 @@ public class PlayerAudioManager : MonoBehaviour
     public AudioClip gem;
     public AudioClip pageflip;
     public AudioClip wallSlide;
+    public AudioClip oof;
 
     private AudioSource source;
     private Dictionary<PlayerAudioSignal, AudioTrack> audioStore = new Dictionary<PlayerAudioSignal, AudioTrack>();
@@ -36,6 +37,7 @@ public class PlayerAudioManager : MonoBehaviour
         audioStore.Add(PlayerAudioSignal.GEM, new AudioTrack(gem, false));
         audioStore.Add(PlayerAudioSignal.PAGE_FLIP, new AudioTrack(pageflip, false));
         audioStore.Add(PlayerAudioSignal.WALL_SLIDE, new AudioTrack(wallSlide, true));
+        audioStore.Add(PlayerAudioSignal.OOF, new AudioTrack(oof, false));
     }
 
     public void Signal(PlayerAudioSignal signal)
@@ -57,4 +59,4 @@ public class PlayerAudioManager : MonoBehaviour
     }
 }
 
-public enum PlayerAudioSignal { LIGHT_FOOTSTEP, BIG_FOOTSTEP, JUMP, LAND, GEM, PAGE_FLIP, WALL_SLIDE }
+public enum PlayerAudioSignal { LIGHT_FOOTSTEP, BIG_FOOTSTEP, JUMP, LAND, GEM, PAGE_FLIP, WALL_SLIDE, OOF }
