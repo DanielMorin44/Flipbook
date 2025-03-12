073c77ae773a5d4a684e973b8d68e0c20bf6f6dc
ItsDobes <vikingdobes@gmail.com>
2025-03-12 01:08:29 -0400

    Added tutorial signs
    Tutorial signs explain basic elements of the gameplay

--- Assets/Scripts/InGameMenus/HUDController.cs
+++ Assets/Scripts/InGameMenus/HUDController.cs
@@ -6,6 +6,7 @@ public class HUDController : MonoBehaviour
 {
     public Text numKeysText;
     public TextMeshProUGUI pageNumberText;
+    public TextMeshProUGUI tipText;
     public PlayerController player;
     public LevelManager levelManager;
 
@@ -13,5 +14,17 @@ public class HUDController : MonoBehaviour
     void Update()
     {
         pageNumberText.text = "Page: " + (levelManager.GetOpenedPage()+1).ToString();
+    }
+
+    public void ShowTip(string tip)
+    {
+        tipText.SetText(tip);
+        tipText.enabled = true;
+    }
+
+    public void HideTip()
+    {
+        tipText.SetText("");
+        tipText.enabled = false;
     }
 }
